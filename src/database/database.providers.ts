238debb429ab0fa4d98c33c31f1b7bc842c80669
jackey <chunkitlau@Chuns-MacBook-Pro.local>
2021-08-20 00:07:35 +0800

    cloud sql setup

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,22 +1,31 @@
-import { __prod__, envPath } from '../constants';
-import { createConnection } from 'typeorm';
-import { config } from 'dotenv-safe';
+import { __prod__ } from '../constants';
+import { createConnection, ConnectionOptions } from 'typeorm';
 
-config({ path: envPath });
+const connectionConfig: ConnectionOptions = {
+  type: 'postgres',
+  url: __prod__ ? undefined : process.env.DATABASE_URL,
+  host: __prod__ ? process.env.DB_HOST : undefined,
+  username: __prod__ ? process.env.DB_USER : undefined,
+  password: __prod__ ? process.env.DB_PASS : undefined,
+  extra: __prod__
+    ? {
+        socketPath: process.env.DB_HOST,
+      }
+    : undefined,
+  logging: !__prod__,
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: false,
+  migrations: [__dirname + '/migrations/*.js'],
+  migrationsRun: true,
+};
 
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: async () => {
-      return await createConnection({
-        type: 'postgres',
-        url: process.env.DATABASE_URL,
-        logging: !__prod__,
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: false,
-        migrations: [__dirname + '/migrations/*.js'],
-        migrationsRun: true,
-      }).catch((err) => console.log(err));
+      return await createConnection(connectionConfig).catch((err) =>
+        console.log(err),
+      );
     },
   },
 ];
