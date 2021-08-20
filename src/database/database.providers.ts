f5377bd5091dc98026440710012e99d67b454195
jackey1510 <jackey@HK010401011.local>
2021-08-20 11:21:30 +0800

    fix migration paths

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,30 +1,46 @@
 import { __prod__ } from '../constants';
 import { createConnection, ConnectionOptions } from 'typeorm';
+import 'dotenv-safe/config';
 
-const connectionConfig: ConnectionOptions = {
+var connectionConfig: ConnectionOptions = {
   type: 'postgres',
-  url: __prod__ ? undefined : process.env.DATABASE_URL,
-  host: __prod__ ? process.env.DB_HOST : undefined,
-  username: __prod__ ? process.env.DB_USER : undefined,
-  password: __prod__ ? process.env.DB_PASS : undefined,
-  extra: __prod__
-    ? {
-        socketPath: process.env.DB_HOST,
-      }
-    : undefined,
+  url: process.env.DATABASE_URL,
   logging: true,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: false,
-  migrations: [__dirname + '/migrations/*.js'],
+  migrations: [__dirname + '/../migrations/*.js'],
   migrationsRun: true,
 };
+
+if (__prod__) {
+  connectionConfig = {
+    type: 'postgres',
+    host: process.env.DB_HOST as string,
+    username: process.env.DB_USER as string,
+    password: process.env.DB_PASS as string,
+    extra: {
+      socketPath: process.env.DB_HOST!,
+    },
+    logging: true,
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    synchronize: false,
+    migrations: [__dirname + '/../migrations/*.js'],
+    migrationsRun: true,
+  };
+}
 
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: async () => {
-      const conn = await createConnection(connectionConfig);
-      return conn;
+      console.log(connectionConfig);
+      try {
+        const conn = await createConnection(connectionConfig);
+        return conn;
+      } catch (error) {
+        console.error(error);
+        return;
+      }
     },
   },
 ];
